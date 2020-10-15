941a700cd3d0835145a4e99f48ec212f33ef8a88
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2020-10-15 12:20:53 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+// index.php
+
+require_once 'car.php';
+require_once 'bicycle.php';
+require_once 'vehicle.php';
+require_once 'truck.php';
+require_once 'HighWay.php';
+require_once 'MotorWay.php';
+require_once 'PedestrianWay.php';
+require_once 'ResidentialWay.php';
+
+// No testing demanded, but all necessary php files are implemented in case there is any doubt
+// in the eyes of the beholder of this code
